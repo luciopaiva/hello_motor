57d31e183a91ff7b0a32dc5e6f71be4d442e3de0
Lucio Paiva <luciopaiva@gmail.com>
2015-01-26 12:06:12 -0200

    Find example

--- hello/async.py
+++ hello/async.py
@@ -62,6 +62,8 @@ class HelloMotor():
         print('Stopped')
 
     def bulk_insert_with_future_test(self):
+        """ Bulk insert example.
+        """
         @gen.coroutine
         def bulk_insert():
             print('Starting bulk async insert')
@@ -79,8 +81,42 @@ class HelloMotor():
         self.ioloop.run_sync(bulk_insert)
         print('Stopped')
 
+    def find_one_potato(self):
+        """ Find One example.
+        """
+        @gen.coroutine
+        def find_one():
+            print('Starting search for a potato')
+            document = yield self.db.potato.find_one({'number': 1})
+            print('A potato was found (id: {})'.format(document['_id']))
+
+        self.ioloop.run_sync(find_one)
+        print('Stopped')
+
+    def find_some_potatoes(self):
+        """ Find example. Shows how to use count() too.
+        """
+        @gen.coroutine
+        def find_some():
+            print('Starting search for some potatoes')
+            cursor = self.db.potato.find({'number': {'$gt': 8}})
+            """ :type: motor.MotorCursor
+            """
+            while (yield cursor.fetch_next):
+                potato = cursor.next_object()
+                print('A potato was found (id: {})'.format(potato['_id']))
+            count = yield cursor.count()
+            print('Potatoes found: {}'.format(count))
+
+        self.ioloop.run_sync(find_some)
+        print('Stopped')
+
+
 
 if __name__ == '__main__':
+    hello = HelloMotor()
     # HelloMotor().insert_with_callback_test()
     # HelloMotor().insert_with_future_test()
-    HelloMotor().bulk_insert_with_future_test()
+    # HelloMotor().bulk_insert_with_future_test()
+    # hello.find_one_potato()
+    hello.find_some_potatoes()
